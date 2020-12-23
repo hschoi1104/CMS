5c10fdf71b428f2ef29dc9d99614aedd617a99f9
hschoi1104 <hschoi1104@gmail.com>
2020-12-23 13:34:58 +0900

    Add module dotenv, winston, babel, mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import { logger } from './config/winston';
+import path from "path";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import indexRouter from "./routes/index";
 
-var indexRouter = require('./routes/index');
 
 var app = express();
+dotenv.config();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+// Connect Mongodb
+const db = mongoose.connection;
+db.on("error", console.error);
+db.once("open", function () {
+  logger.info("Connected to mongod server");
+});
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 module.exports = app;
