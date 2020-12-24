e25d530d71a466080b7d2267d23afdb55e97467b
hschoi1104 <hschoi1104@gmail.com>
2020-12-24 17:19:28 +0900

    Create user.dao.js
    Add user create func

---
+++ backend/dao/user.dao.js
@@ -0,0 +1,12 @@
+import User from '../model/User';
+
+export class UserDao {
+  static create = async (req) => {
+    const user = new User({
+      id: req.id,
+      password: req.password,
+      name: req.name,
+    });
+    return user.save();
+  };
+}
