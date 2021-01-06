9fd991098a68b4209a797e5f813ae0ff1808a15a
hschoi1104 <hschoi1104@gmail.com>
2021-01-06 12:22:17 +0900

    Create Footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+	<v-card>
+		<v-footer class="absolute" :padless="true">
+			<v-card flat tile width="100%" color="#2F5FD2" class="text-center">
+				<v-card-text class="white--text">
+					<strong
+						>ⓒ 2021. 모바일 앱 개발 협동 조합 all rights reserved.
+					</strong>
+				</v-card-text>
+			</v-card>
+		</v-footer>
+	</v-card>
+</template>
+<script>
+export default {
+	data: () => ({
+		icons: ['mdi-home', 'mdi-email', 'mdi-calendar', 'mdi-delete'],
+		items: ['default', 'absolute', 'fixed'],
+		variant: 'default',
+	}),
+	computed: {
+		localAttrs() {
+			const attrs = {};
+
+			if (this.variant === 'default') {
+				attrs.absolute = false;
+				attrs.fixed = false;
+			} else {
+				attrs[this.variant] = true;
+			}
+			return attrs;
+		},
+	},
+};
+</script>
