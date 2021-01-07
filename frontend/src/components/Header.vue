1d27eb9a864b338dfecdab48df6037817e980e3a
hschoi1104 <hschoi1104@gmail.com>
2021-01-07 16:17:31 +0900

    Update Header.vue
    Close #24

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,40 +1,34 @@
 <template>
 	<div>
-		<v-app-bar color="#2F5FD2" class="white--text">
-			<v-toolbar-title> <strong>동화 히어로 CMS</strong></v-toolbar-title>
+		<v-container>
+			<v-app-bar absolute color="#2F5FD2" class="white--text">
+				<v-toolbar-title> <strong>동화 히어로 CMS</strong></v-toolbar-title>
 
-			<v-spacer></v-spacer>
+				<v-spacer></v-spacer>
 
-			<v-row>
-				<v-card flat color="#2F5FD2" @click="$router.push('/object/upload')">
-					<v-card-text class="white--text">오브젝트 업로드</v-card-text></v-card
-				>
-			</v-row>
-			<v-row>
-				<v-card flat color="#2F5FD2" @click="$router.push('/object/manage')">
-					<v-card-text class="white--text">오브젝트 관리</v-card-text></v-card
-				>
-			</v-row>
-			<v-row>
-				<v-card flat color="#2F5FD2" @click="$router.push('/signup')">
-					<v-card-text class="white--text">회원가입</v-card-text></v-card
-				>
-			</v-row>
-			<v-row>
-				<v-card flat color="#2F5FD2" @click="$router.push('/login')">
-					<v-card-text class="white--text">로그인</v-card-text></v-card
-				>
-			</v-row>
-		</v-app-bar>
+				<v-row class="justify-end">
+					<v-card flat color="#2F5FD2" @click="$router.push('/object/upload')">
+						<v-card-text class="white--text">오브젝트 업로드</v-card-text>
+					</v-card>
+
+					<v-card flat color="#2F5FD2" @click="$router.push('/object/manage')">
+						<v-card-text class="white--text">오브젝트 관리</v-card-text>
+					</v-card>
+
+					<v-card flat color="#2F5FD2" @click="$router.push('/signup')">
+						<v-card-text class="white--text">회원가입</v-card-text>
+					</v-card>
+
+					<v-card flat color="#2F5FD2" @click="$router.push('/login')">
+						<v-card-text class="white--text">로그인</v-card-text>
+					</v-card>
+				</v-row>
+			</v-app-bar>
+		</v-container>
 	</div>
 </template>
 <script>
 export default {
 	name: 'Home',
-	methods: {
-		routerPush() {
-			this.$router.push('/signup');
-		},
-	},
 };
 </script>
