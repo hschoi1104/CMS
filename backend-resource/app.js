a9b1a3bce7b74fbb247ef7b01c7d57f0c0659bf3
hschoi1104 <hschoi1104@gmail.com>
2021-01-21 00:06:57 +0900

    Add moduel cors

--- backend-resource/app.js
+++ backend-resource/app.js
@@ -6,10 +6,12 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import indexRouter from './routes/index';
+import cors from 'cors';
 
 var app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
