4aa93c99236b795a321c89424e22d151921048b4
hschoi1104 <hschoi1104@gmail.com>
2021-01-20 16:21:20 +0900

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
-	<v-container class="fill-height text-center">
-		<!-- <v-text>Home PAGE</v-text> -->
-		<p>asdadfs</p>
-		<p>{{ accessToken }}</p>
+	<v-container class="fill-height" fluid>
+		<v-row align="center" justify="center">
+			<v-card>
+				<v-card-title class="display-3 text-center">Home Page</v-card-title>
+				<v-card-text
+					class="success text-center display-1"
+					v-if="this.accessToken"
+				>
+					accessToken 저장되어있음
+				</v-card-text>
+				<v-card-text
+					class="error text-center display-1"
+					v-if="!this.accessToken"
+				>
+					accessToken 없음
+				</v-card-text>
+			</v-card>
+		</v-row>
 	</v-container>
 </template>
 
