823d126e80a46fb388c7d5df796a185099e137a2
hschoi1104 <hschoi1104@gmail.com>
2021-01-25 19:00:18 +0900

    Set cors

--- backend-auth/app.js
+++ backend-auth/app.js
@@ -11,7 +11,12 @@ import cors from 'cors';
 var app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
